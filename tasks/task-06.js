578ecc1ab4bc65beee72a3e139bb6c37a29db46b
Serhii Bondar <sergiibondar1979@gmail.com>
2020-10-11 19:29:56 +0300

    Create task-06

--- tasks/task-06.js
+++ tasks/task-06.js
@@ -1,7 +1,45 @@
-const hotel = {
-  name: "Resort Hotel",
-  stars: 5,
-  capacity: 100,
-};
-const key = "Радар";
-console.log(key in hotel);
+function calculateTotalPrice(array, prop) {
+  "use strict";
+  // Write code under this line
+  let totalCost = 0;
+
+  for (const arr of array) {
+    // const keys = Object.keys(arr);
+    const values = Object.values(arr);
+
+    if (values[0] === prop) {
+      totalCost += values[1] * values[2];
+    }
+
+    // for (const key of keys) {
+    //   if (key[] === prop) {
+    //     totalCost += keys[1] * keys[2];
+    //   }
+    // }
+  }
+
+  return totalCost;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+console.log(calculateTotalPrice(products, "Радар"));
+// 9080
+
+//console.log(calculateTotalPrice(products, 'Сканер'));
+// 10200
+
+//console.log(calculateTotalPrice(products, 'Захват'));
+// 2400
+
+//console.log(calculateTotalPrice(products, 'Дроид'));
+// 2800
